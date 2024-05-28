cc1724ed97b54c41168a245812da6bbac885b477
devSamuel <samueldflima@proton.me>
2024-05-27 22:35:23 -0300

    Refactor

--- src/controllers/financial_controller.clj
+++ src/controllers/financial_controller.clj
@@ -1,10 +1,13 @@
 (ns controllers.financial-controller
-  (:require [compojure.core :refer [defroutes GET POST]]
+  (:require [compojure.core :refer [defroutes DELETE GET POST]]
             [external.json :refer [as-json]]
             [repositories.financial-repository :refer [create-transaction
+                                                       delete-transactions
                                                        read-transactions]]))
 
 (defroutes financial-routes
-  (GET "/read-finances" [] (as-json (read-transactions)))
+  (GET "/read-finances" [] (-> (read-transactions)
+                               (as-json)))
   (POST "/create-finance" request (-> (create-transaction (:body request))
-                                      (as-json))))
+                                      (as-json)))
+  (DELETE "/delete-finances" [] (delete-transactions)))
