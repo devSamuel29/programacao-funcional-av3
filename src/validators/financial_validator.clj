d765b3c2ea2f804305ac53f5de413748c14f6539
devSamuel <samueldflima@proton.me>
2024-06-04 17:37:44 -0300

    change to parse float

--- src/validators/financial_validator.clj
+++ src/validators/financial_validator.clj
@@ -3,7 +3,7 @@
 (defn valid-transaction? [request]
   (let [allowed-keys #{:value :type}]
     (and (contains? request :value)
-         (pos? (Double/parseDouble (:value request)))
+         (pos? (Float/parseFloat (:value request)))
          (contains? request :type)
          (or (= (:type request) "Receita")
              (= (:type request) "Despesa"))
